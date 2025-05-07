71cc22a8ee7b68be4403caf27829b32afd0af29e
Joaquin Roca <joaquinrocarod@gmail.com>
2025-05-07 19:56:11 +0200

    Testing eslint+gitactions

--- src/middleware/routeNotFound.ts
+++ src/middleware/routeNotFound.ts
@@ -1,11 +1,7 @@
 import express, { Request, Response } from 'express';
 
 
-const hola: any = 'mundo'; // ❌ viola @typescript-eslint/no-explicit-any
-const hola2: any = 'mundo'; // ❌ viola @typescript-eslint/no-explicit-any
-const hola3: any = 'mundo'; // ❌ viola @typescript-eslint/no-explicit-any
-const hola4: any = 'mundo'; // ❌ viola @typescript-eslint/no-explicit-any
-const hola5: any = 'mundo'; // ❌ viola @typescript-eslint/no-explicit-any
+const holaA: any = 'mundo'; // ❌ viola @typescript-eslint/no-explicit-any
 
 export function routeNotFound(req: Request, res: Response) {
     const error = new Error('Route Not Found');
