226911b75a1479592bdcaf770bc67ad5b0077ad0
Joaquin Roca <joaquinrocarod@gmail.com>
2025-05-07 19:36:00 +0200

    Testing eslint+gitactions

--- src/middleware/routeNotFound.ts
+++ src/middleware/routeNotFound.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import { Request, Response } from 'express';
 
 export function routeNotFound(req: Request, res: Response) {
     const error = new Error('Route Not Found');
